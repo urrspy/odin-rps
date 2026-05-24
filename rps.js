92881d37640d076a3e4551b1ad50a45b011438fc
urrspy <267764548+urrspy@users.noreply.github.com>
2026-05-24 21:32:27 +0300

    created commputer choice logic

--- rps.js
+++ rps.js
@@ -1,1 +1,19 @@
-console.log("hi");
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+	let rng = Math.floor(Math.random() * 10);
+	console.log(rng);
+	let choice = ""
+
+	if ( rng < 3 ) {
+		return choice = "rock";
+	} else if ( rng < 6 ) {
+		return choice = "paper";
+	} else {
+		return choice = "scissors";
+	}
+}
+
+function getHumanChoice() {
+
+}
